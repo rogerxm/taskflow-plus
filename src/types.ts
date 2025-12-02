8d4d399ea235587edec2694ab782580f7cc35222
Roger Montes <rogerxmontes@gmail.com>
2025-12-02 13:49:41 -0600

    add types for Todo, FilterType, and TodosState interfaces

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+  createdAt: number;
+}
+
+export type FilterType = "all" | "active" | "completed";
+
+export interface TodosState {
+  items: Todo[];
+  filter: FilterType;
+}
