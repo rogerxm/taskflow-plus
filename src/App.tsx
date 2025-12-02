ffeda8f4a71b6aab306a9be735da5e86dce730c9
Roger Montes <rogerxmontes@gmail.com>
2025-12-02 13:50:02 -0600

    add Redux provider and AppContent component to main App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <></>;
-}
+import { Provider } from "react-redux";
+import { store } from "./redux/todoSlice";
+import AppContent from "./components/layout/AppContent";
 
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
+  );
+}
